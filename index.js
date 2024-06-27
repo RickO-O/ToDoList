344f171b29a978f5792b98cd831772f6d19d996f
RickO-O <Ricktheyay@gmail.com>
2024-06-27 14:57:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const path = require('path');
 
 
-app.use(express.json());
-
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+app.use(express.static(path.join(__dirname, 'public'))); 
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'index.html')); }); 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
